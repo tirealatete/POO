ee9539a9ffe712b714ca06da590e4a7fadc54f51
tirealatete <tirealatete@yahoo.fr>
2013-03-22 14:37:31 +0100

    Version 1.0

---
+++ Membre.class.php
@@ -0,0 +1,29 @@
+<?php
+class Membre
+{
+	private $pseudo;
+    private $email;
+    private $signature;
+    private $actif;
+
+    public function envoyerEMail($titre, $message)
+    {
+        mail($this->email, $titre, $message);
+    }
+     
+    public function bannir()
+    {
+        $this->actif = false;
+        $this->envoyerEMail('Vous avez été banni', 'Ne revenez plus !');
+    }
+    
+    public function getPseudo()
+    {
+    	return $this->pseudo;
+    }
+
+    public function setPseudo($nouveauPseudo)
+    {
+    	$this->pseudo = $nouveauPseudo;
+    }
+}
